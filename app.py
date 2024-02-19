84f664c1a6afe9bedd307f3617d763d980f3b9c6
Taciano da Hora <taciano.dev@outlook.com>
2024-02-19 17:06:31 -0300

    feat(database): configure SQLite database connection

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
